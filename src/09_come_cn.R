7ac97ffffb997ab5c283a5afdb7195df52695a61
marromesc <mrescorza@gmail.com>
2023-01-12 10:09:31 +0000

    add come mut and cn

--- src/09_come_cn.R
+++ src/09_come_cn.R
@@ -30,7 +30,6 @@ GenesPanel <- readRDS('./data/GenesPanel.RDS')
 oncogene <- read.csv(oncogene_datapath)
 ts <- read.csv(ts_datapath)
 
-
 # Set up mutation data ----------------------------------------------------
 
 all_muts <- eventDataFrame[eventDataFrame$gene.knowngene %in% GenesPanel & eventDataFrame$patient_id %in% SampleSheet$patient_id,] #removing contralaterals
@@ -39,6 +38,31 @@ geneMatrix[geneMatrix > 0 ] = 1
 
 events <- discover.matrix(geneMatrix)
 subset <- rowSums(geneMatrix) > 3 # remove mutations affecting less than 3 samples
+
+# We make a selection of genes that will be used in the pairwise co-occurrence and mutual exclusivity 
+# analyses. Genes are selected if they are (1) located in a recurrently altered copy number segment and 
+# included in a list of known cancer genes, or (2) included in a list of mutational driver genes.
+
+# load cancer gene list from Bushman lab
+cancer_gene <- read_tsv("http://www.bushmanlab.org/assets/doc/allonco_20130923.tsv")[['symbol']]
+
+# load High-confidence mutational drivers list from Tamborero, D. et al. Comprehensive identification of mutational cancer driver genes across 12 tumor types. Sci Rep 3, 2650 (2013)
+download.file("https://static-content.springer.com/esm/art%3A10.1038%2Fsrep02650/MediaObjects/41598_2013_BFsrep02650_MOESM2_ESM.zip", 'srep02650-s2.zip')
+unzip('srep02650-s2.zip')
+file.remove('srep02650-s2.zip')
+mut_genes <- read_csv('srep02650-s3.csv')
+high_conf_drivers <- mut_genes[mut_genes$`Putative Driver Category` == 'High Confidence Driver',][['Gene Symbol']]
+
+# select genes
+selected_genes <- unique(c(cancer_gene, high_conf_drivers))
+
+subset[which(!(names(subset) %in% selected_genes))] <- F
+
+# Set up copy number data + mutations --------------------------------------
+
+geneMatrixCN <- t(addCN2Muts(gisticRegs=gisticRegs, geneMatrix=t(geneMatrix), SampleSheet_CN=SampleSheet_CN))
+events <- discover.matrix(geneMatrixCN)
+subset <- rowSums(geneMatrixCN) > 3 # remove mutations affecting less than 3 samples
 
 # We make a selection of genes that will be used in the pairwise co-occurrence and mutual exclusivity 
 # analyses. Genes are selected if they are (1) located in a recurrently altered copy number segment and 
@@ -74,7 +98,7 @@ result.mutex2 <- as.data.frame(result.mutex)
 
 #heatmap
 
-corMat <- cor(t(geneMatrix[subset,]), method = c("pearson", "kendall", "spearman")[1])*0
+corMat <- cor(t(geneMatrixCN[subset,]), method = c("pearson", "kendall", "spearman")[1])*0
 for (i in 1:nrow(corMat)) {
   for (j in 1:ncol(corMat)) {
     Ind <- which((result.mutex1$gene1==rownames(corMat)[i] & result.mutex1$gene2==colnames(corMat)[j]) | (result.mutex1$gene2==rownames(corMat)[i] & result.mutex1$gene1==colnames(corMat)[j]))
@@ -95,7 +119,7 @@ for (i in 1:nrow(corMat)) {
 #corMat <- corMat[,colSums(corMat) != 0]
 #corMat <- corMat[rowSums(corMat) != 0,]
 
-pdf('./results/come_DISCOVER.pdf', width = 10, height = 10)
+pdf('./results/come_DISCOVER_cn.pdf', width = 10, height = 10)
 Heatmap(corMat,       
         col=colorRamp2(c(-3,0,3), c("darkolivegreen", 'white', "chocolate1")),
         cluster_columns = F, 
@@ -115,13 +139,10 @@ dev.off()
 
 # Pairwise fisher test ----------------------------------------------------
 
-mutMat <- t(geneMatrix[subset,])
+mutMat <- t(geneMatrixCN[subset,])
 
-pdf('./results/come_fisher.pdf')
-fishertest <- somaticInteractions(mutMat)
+pdf('./results/come_fisher_cn.pdf')
+fishertest <- somaticInteractions(mutMat, geneOrder = rev(names(subset)[subset]))
 dev.off()
 
-write.csv(fishertest, './results/come_fisher_test.csv', quote = F, row.names = F)
-
-
-
+write.table(fishertest, './results/come_fisher_test_cn.tsv', sep='\t', quote = F, row.names = F)
